f302323e7b9ee884bf8d050e2c7b1ca8d1e866cd
kanakana_rcj <nakagawa.dadada5@gmail.com>
2024-07-20 14:29:52 +0900

    add next-app with google map

---
+++ toilet-dash/src/providers/map-provider.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import React, {ReactNode} from "react";
+import {Libraries, useJsApiLoader} from '@react-google-maps/api';
+
+const libraries = ['places', 'drawing', 'geometry'];
+
+export default function MapProvider({children}: { children: ReactNode}) {
+  const {isLoaded: scriptLoaded, loadError} = useJsApiLoader({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAP_API as string,
+    libraries: libraries as Libraries,
+  });
+
+  if(loadError) return <p>Encounted error while loading google maps</p>
+
+  if(!scriptLoaded) return <p>Map Script is loading ...</p>
+
+  return children;
+}
