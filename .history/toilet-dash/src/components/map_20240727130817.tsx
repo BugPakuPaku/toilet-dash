96e7643bafac7c43f21d02f1359bec1bf9eeb9c1
kofe <t2312121@gl.cc.uec.ac.jp>
2024-07-27 13:13:18 +0900

    nochanges

---
+++ .history/toilet-dash/src/components/map_20240727130817.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import {GoogleMap} from "@react-google-maps/api";
+
+export const defaultMapContainerStyle = {
+  width: '100%',
+  height: '80vh',
+  borderRadius: '15px 0px 0px 15px',
+};
+
+const defaultMapCenter = {
+  lat: 35.655439,
+  lng: 139.54368
+}
+
+const defaultMapZoom = 18
+
+const defaultMapOptions = {
+  zoomControl: true,
+  tilt: 0,
+  gestureHandling: 'auto',
+  mapTypeId: 'roadmap',  //sateliteなら衛星写真に
+};
+
+const MapComponent = () => {
+  return (
+    <div className = "w-full">
+      <GoogleMap 
+      mapContainerStyle={defaultMapContainerStyle}
+      center={defaultMapCenter}
+      zoom={defaultMapZoom}
+      options={defaultMapOptions}
+      >
+      </GoogleMap>
+    </div>
+  )
+};
+
+export { MapComponent };
