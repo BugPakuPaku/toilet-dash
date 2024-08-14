fd61605152601fe3409870c6d2cb99a624fbc59b
shiragicord <39217112+shiragicord@users.noreply.github.com>
2024-08-14 11:13:07 +0900

    Revert "Merge branch 'feature-issue-10'"
    This reverts commit 767f5e0887dcdbfb0a349b68a1beceee9b715de5, reversing
    changes made to 2907fbf12f4196427806d8dd23ecf39c706d19e0.

--- toilet-dash/src/app/page.tsx
+++ toilet-dash/src/app/page.tsx
@@ -1,22 +1,10 @@
-'use client'
-
 import MapProvider from "@/providers/map-provider";
-import { MapComponent, PCMapComponent, SPMapComponent } from "@/components/MapComponent";
-import Box from '@mui/material/Box'
-import Link from "next/link"
+import { MapComponent } from "@/components/map";
 
 export default function Page() {
   return (
     <MapProvider>
-      <Box sx={{ display: { xs: 'block', md: 'none' }}}>
-        <SPMapComponent/>
-        <span>
-          <Link href={`/list`}>list</Link>
-        </span>
-      </Box>
-      <Box sx={{ display: { xs: 'none', md: 'block' }}}>
-        <PCMapComponent/>
-      </Box>
+      <MapComponent/>
     </MapProvider>
   );
 }
